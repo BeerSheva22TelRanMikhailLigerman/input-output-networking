310757cc2e17ea5137c53e0ffc6c25abb8b66f44
user <user@192.168.1.194>
2023-04-15 13:11:15 +0300

    clear

---
+++ src/MyExamples.java
@@ -0,0 +1,13 @@
+
+public class MyExamples {
+	
+
+	public static void main(String[] args) {
+		int id = 1;
+		int id2 = 12345;
+		System.out.println(id + "*".repeat(5 - Integer.toString(id).length())); 
+		System.out.println(id2 + "*".repeat(5 - Integer.toString(id2).length()));
+
+	}
+
+}

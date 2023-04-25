1f44dd77c1d95d8b9a69b68b1866894f911c96a1
user <user@WIN-58FL53FAT7O>
2023-04-25 12:40:59 +0300

    25-4-2023 before CW54

--- src/telran/net/TcpServer.java
+++ src/telran/net/TcpServer.java
@@ -1,10 +1,15 @@
 package telran.net;
 import java.io.IOException;
 import java.net.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class TcpServer implements Runnable {
 private Protocol protocol;
 private int port;
+//CW54
+private ExecutorService executor;
+
 private ServerSocket serverSocket;
 
 	@Override
@@ -14,7 +19,8 @@ private ServerSocket serverSocket;
 			try {
 				Socket socket = serverSocket.accept();
 				TcpServerClient serverClient = new TcpServerClient(socket, protocol);
-				serverClient.run();
+				//serverClient.run();
+				executor.execute(serverClient); //CW54
 			} catch (Exception e) {
 				System.out.println(e.toString());
 			}
@@ -25,5 +31,13 @@ private ServerSocket serverSocket;
 		this.protocol = protocol;
 		this.port = port;
 		serverSocket = new ServerSocket(port);
+		
+		//CW54
+		int nThreads = Runtime.getRuntime().availableProcessors();
+		System.out.println("Number threads in Thread pool is: " + nThreads);
+		executor = Executors.newFixedThreadPool(nThreads);		
+	}
+	public void shutdown() {
+		//TODO
 	}
 }
